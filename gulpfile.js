1e19fda038ee288e65e629108e7747940aee0b62
Joseph Duffy <JosephDuffy@me.com>
2015-11-27 20:22:00 +0000

    Reduced the size of the output CSS by removing unused rules
    Reduced the gzipped file size from 5.7KB to 2.2KB
    The sitemapper module is used to retrieve the site's sitemap.xml file and the uncss module then scans all of the pages and removes any unused rules.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,42 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var uncss = require('gulp-uncss');
 var nano = require('gulp-cssnano');
+var sitemap = require('sitemapper');
 
 var compileDirectories = ['./public/css/**/*.scss', '!./public/css/bootstrap/**/*'];
 var watchDirectories = ['./public/css/**/*.scss', './gulpfile.js'];
 var outputDirectory = './public/css';
 
-gulp.task('compile', function() {
-    gulp.src('./public/css/styles.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(nano())
-        .pipe(sourcemaps.write('../css'))
-        .pipe(gulp.dest(outputDirectory));
+gulp.task('compile', function(done) {
+    // Try and load the URLs from the sitemap.xml file so that uncss can
+    // remove any unused rules
+    // If the server is not running at this address the uncss stage will
+    // not doing anything and all CSS rules will be kept intact
+    sitemap.getSites('http://localhost:2368/sitemap.xml', function(err, sites) {
+        if (err) {
+            done(err);
+        } else {
+            gulp.src('./public/css/styles.scss')
+                .pipe(sourcemaps.init())
+                .pipe(sass().on('error', sass.logError))
+                .pipe(autoprefixer({
+                    browsers: ['last 2 versions'],
+                    cascade: false
+                }))
+                .pipe(uncss({
+                    html: sites
+                }))
+                .pipe(nano())
+                .pipe(sourcemaps.write('../css'))
+                .pipe(gulp.dest(outputDirectory));
+                done();
+        }
+    });
 });
 
 gulp.task('watch', function() {
