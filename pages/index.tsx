1db9991c99bba55b67422a55878686d04c33e142
Joseph Duffy <commits@josephduffy.co.uk>
2019-12-06 22:42:40 +0000

    Add global header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { NextPage } from 'next';
+import Header from '../components/Header';
 
-const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <h1>Hello world! - user agent: {userAgent}</h1>
+const Index: NextPage = () => (
+  <div>
+    <Header />
+    <p>Hello Next.js</p>
+  </div>
 );
 
-Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
-  return { userAgent };
-};
+export default Index;
 
-export default Home;
