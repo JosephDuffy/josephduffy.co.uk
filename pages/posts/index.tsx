cf8782e604ec864a2302f1ede0dceb4f6a2c9cbf
Joseph Duffy <commits@josephduffy.co.uk>
2020-01-20 13:36:36 +0000

    Add index for blog posts

---
+++ pages/posts/index.tsx
@@ -0,0 +1,38 @@
+import { NextPage } from 'next'
+import Page from '../../layouts/main'
+import postsLoader from '../../data/loaders/PostsLoader'
+import BlogPost from "../../models/BlogPost"
+import BlogPostPreview from '../../components/BlogPostPreview'
+
+interface Props {
+  posts: BlogPost[]
+}
+
+const PostPage: NextPage<Props> = (props) => {
+  const { posts } = props
+  return (
+    <Page>
+      {
+        posts.map(post => {
+          return <BlogPostPreview post={post} key={post.slug}/>
+        })
+      }
+    </Page>
+  )
+}
+
+interface StaticProps {
+  props: Props,
+}
+
+export async function unstable_getStaticProps(): Promise<StaticProps> {
+  const posts = await postsLoader.getPosts()
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default PostPage
