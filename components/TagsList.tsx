b03a82d32d4e1f92c8b41b51a5bb695e07feaa5c
Joseph Duffy <commits@josephduffy.co.uk>
2020-01-21 00:28:44 +0000

    Style tags list

--- components/TagsList.tsx
+++ components/TagsList.tsx
@@ -7,20 +7,52 @@ interface Props {
 
 const TagsList: FunctionComponent<Props> = ({ tags }) => {
   return (
-    <ul>
-      {
-        tags.map(tag => {
-          const tagURL = `/tags/${tag}`
-          return (
-            <li key={tag}>
-              <Link href={tagURL}>
-                <a>{ tag }</a>
-              </Link>
-            </li>
-          )
-        })
-      }
-    </ul>
+    <div>
+      <div>
+        <span className="label">Tags:</span>
+        <ul>
+          {
+            Array.from(tags.entries()).map(entry => {
+              const [index, tag] = entry
+              const tagURL = `/tags/${tag}`
+              return (
+                <li key={tag}>
+                  <Link href={tagURL}>
+                    <a>{ tag }</a>
+                  </Link>
+                  {index !== tags.length - 1 &&
+                  <span>,</span>
+                  }
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+      <style jsx>{`
+        div {
+          display: flex;
+          flex-wrap: wrap;
+        }
+
+        span.label {
+          margin-right: 8px;
+        }
+
+        ul {
+          display: flex;
+          flex-wrap: wrap;
+          list-style-type: none;
+          padding: 0;
+          margin: 0;
+        }
+
+        li {
+          margin-right: 8px;
+          display: inline;
+        }
+      `}</style>
+    </div>
   )
 };
 
