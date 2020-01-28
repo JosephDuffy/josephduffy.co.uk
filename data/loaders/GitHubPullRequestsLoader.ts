658c6f28a65afa4d3c8f6142f7be978b54f614f7
Joseph Duffy <commits@josephduffy.co.uk>
2020-01-28 15:02:23 -0800

    Add GitHubPullRequestLoader

---
+++ data/loaders/GitHubPullRequestsLoader.ts
@@ -0,0 +1,132 @@
+import ApolloClient from "apollo-client"
+import gql from "graphql-tag"
+import fetch from "node-fetch"
+import { createHttpLink } from "apollo-link-http"
+import { InMemoryCache, ObjectCache } from "apollo-cache-inmemory"
+import { Entry } from "./Entry"
+
+const query = gql`
+  query {
+    user(login: "josephduffy"){
+      pullRequests(first: 100, orderBy: { field: CREATED_AT, direction: DESC }) {
+        nodes {
+          title
+          body
+          url
+          createdAt
+          repository {
+            nameWithOwner
+            owner {
+              login
+            }
+            repositoryTopics(first: 100) {
+              nodes {
+                topic {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+interface QueryResult {
+  user: {
+    pullRequests: {
+      nodes: PullRequest[]
+    }
+  }
+}
+
+interface PullRequest {
+  title: string
+  body: string
+  url: string
+  createdAt: string
+  repository: {
+    nameWithOwner: string
+    owner: {
+      login: string
+    }
+    repositoryTopics: {
+      nodes: {
+        topic: {
+          name: string
+        }
+      }[]
+    }
+  }
+}
+
+export function isGitHubPullRequest(object: any): object is GitHubPullRequest {
+  return (
+    typeof object.name === "string" &&
+    (typeof object.description === "string" || object.description === null) &&
+    object.hasOwnProperty("date") &&
+    typeof object.url === "string" &&
+    Array.isArray(object.tags)
+  )
+}
+
+export interface GitHubPullRequest extends Entry {
+  name: string
+  description: string
+  url: string
+  repoName: string
+  date: string
+  tags: string[]
+}
+
+export class GitHubPullRequestLoader {
+  private cachedPullRequests?: GitHubPullRequest[]
+
+  async getPullRequests(forceRefresh: boolean = false): Promise<GitHubPullRequest[]> {
+    if (!forceRefresh && this.cachedPullRequests) {
+      console.debug("Using cached GitHub pull requests")
+      return this.cachedPullRequests
+    }
+
+    const link = createHttpLink({
+      uri: "https://api.github.com/graphql",
+      fetch: fetch as any,
+      headers: {
+        Authorization: `bearer ${process.env["GITHUB_OAUTH_TOKEN"]}`,
+      },
+    })
+    const client = new ApolloClient({
+      link: link,
+      cache: new InMemoryCache(),
+    })
+    console.debug("Loading GitHub pull requests")
+    const result = await client.query({ query })
+    if (result.errors) {
+      console.error("Error loading GitHub pull requests", result.errors)
+      throw result.errors
+    }
+
+    const pullRequestTags = ["open-source"]
+    const data = result.data as QueryResult
+    const pullRequests = data.user.pullRequests.nodes.filter(pullRequest => pullRequest.repository.owner.login !== "JosephDuffy").flatMap(pullRequest => {
+      const repoTags = pullRequestTags.concat(
+        pullRequest.repository.repositoryTopics.nodes.map(node => node.topic.name),
+      )
+      return {
+        name: pullRequest.title,
+        description: pullRequest.body,
+        url: pullRequest.url,
+        repoName: pullRequest.repository.nameWithOwner,
+        date: pullRequest.createdAt,
+        tags: pullRequestTags.concat(pullRequest.repository.repositoryTopics.nodes.map(node => node.topic.name)),
+      }
+    })
+    this.cachedPullRequests = pullRequests
+    return pullRequests
+  }
+}
+
+const loader = new GitHubPullRequestLoader()
+
+export default loader
