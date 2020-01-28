e92c9c3d6e3bc42e03b8756120cac10ccfccf5b7
Joseph Duffy <commits@josephduffy.co.uk>
2020-01-28 14:02:22 -0800

    Begin styling of entry previews

--- components/EntryPreviews.tsx
+++ components/EntryPreviews.tsx
@@ -1,5 +1,5 @@
 import { Entry } from "../data/loaders/Entry";
-import { FunctionComponent } from 'react';
+import { FunctionComponent, Fragment, Component } from 'react';
 import { isGitHubRelease } from '../data/loaders/GitHubReleasesLoader';
 import { isStackOverflowEntry } from '../data/loaders/StackOverflowLoader';
 import { isBlogPost } from '../models/BlogPost';
@@ -14,24 +14,56 @@ interface Props {
   entries: Entry[]
 }
 
-const EntryPreviews: FunctionComponent<Props> = ({ entries }) => {
-  return (
-    <div>
-      {entries.map(entry => {
-        if (isCombinedEntry(entry)) {
-          return <CombinedEntryPreview entry={entry} key={entry.title}/>
-        } else if (isGitHubRelease(entry)) {
-          return <GitHubReleasePreview release={entry} key={entry.url}/>
-        } else if (isStackOverflowEntry(entry)) {
-          return <StackOverflowEntryPreview entry={entry} key={entry.url}/>
-        } else if (isBlogPost(entry)) {
-          return <BlogPostPreview post={entry} key={entry.url}/>
-        } else {
-          throw `Unknown entry of type ${typeof entry}: ${inspect(entry)}`
-        }
-      })}
-    </div>
-  )
+class EntryPreviews extends Component<Props> {
+
+  render() {
+    const { entries } = this.props
+    return (
+      <Fragment>
+        {entries.map(entry => {
+          let key: string
+          if (entry.url) {
+            key = entry.url
+          } else if (isCombinedEntry(entry)) {
+            key = entry.title
+          } else {
+            // TODO: Create a better key
+            key = entry.date + entry.tags.concat('')
+          }
+          return (
+            <div className="entry-preview" key={key}>
+                { this.previewForEntry(entry) }
+            </div>
+          )
+        })}
+        <style jsx>{`
+          div :global(h1) {
+            margin: 0.4em 0;
+            font-size: 1.7em;
+          }
+
+          div.entry-preview {
+            margin-top: 24px;
+          }
+        `}</style>
+      </Fragment>
+    )
+  }
+
+  private previewForEntry(entry: Entry) {
+    if (isCombinedEntry(entry)) {
+      return <CombinedEntryPreview entry={entry}/>
+    } else if (isGitHubRelease(entry)) {
+      return <GitHubReleasePreview release={entry}/>
+    } else if (isStackOverflowEntry(entry)) {
+      return <StackOverflowEntryPreview entry={entry}/>
+    } else if (isBlogPost(entry)) {
+      return <BlogPostPreview post={entry}/>
+    } else {
+      throw `Unknown entry of type ${typeof entry}: ${inspect(entry)}`
+    }
+  }
+
 }
 
 export default EntryPreviews;
