d7292e0fca3a7197c45b79eb515ff934c08511ac
Joseph Duffy <commits@josephduffy.co.uk>
2020-03-01 21:07:51 +0000

    Rename GITHUB_OAUTH_TOKEN to GITHUB_ACCESS_TOKEN

--- start-server.sh
+++ start-server.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [[ -z "${GITHUB_OAUTH_TOKEN}" ]]; then
-  echo "GITHUB_OAUTH_TOKEN environment variable is required"
+if [[ -z "${GITHUB_ACCESS_TOKEN}" ]]; then
+  echo "GITHUB_ACCESS_TOKEN environment variable is required"
   exit 1
 fi
 
-docker build -t josephduffy:latest --build-arg GITHUB_OAUTH_TOKEN=$GITHUB_OAUTH_TOKEN .
+docker build -t josephduffy:latest --build-arg GITHUB_ACCESS_TOKEN=$GITHUB_ACCESS_TOKEN .
 echo "Starting server listening on 80 (http) and 443 (https)"
 docker run -p 80:80 -p 443:443 -it josephduffy:latest
 echo "Server stopped"
