052cc2bfc7eddc42dc2fcdcdf05eaf061571f970
Joseph Duffy <commits@josephduffy.co.uk>
2020-03-07 16:39:39 +0000

    Add support for viewing changelogs

--- data/apps/gathered.ts
+++ data/apps/gathered.ts
@@ -98,92 +98,98 @@ const gathered: App = {
   tags: ["gathered"],
   changelogs: [
     {
-      version: "1.0",
-      releaseDate: "2015-01-23",
+      version: "1.2.0",
+      releaseDate: "2016-10-18",
       content: `
-       - Initial release
-      `,
-    },
-    {
-      version: "1.0.1",
-      releaseDate: "2015-11-12",
-      content: `
-      - Removed the Location and Device tabs
-      - Moved the Settings screen to its own tab
-      - Removed the "Disable Device Sleep" option (at Apple's request)
-      - Gathered will now remember which tab you had selected and if you were editing, even when you leave the app closed for a long time
-      - Improved reliability of purchasing and restoring in-app purchases
-      - Fixed multiple crashes on the Settings screen on iOS 7, including:
-
-      - Restoring or purchasing in-app purchases
-      - Tapping on Rate Gathered on iTunes, Feedback, or Privacy Policy
-
-      - Improvements and fixes for iOS 9
-      - Reduced app size
-      - Improved layout of alert to choose how to view a Twitter profile from the About screen
-      - Lots of other little behind the scenes fixes to make Gathered faster and more stable
-      `,
-    },
-    {
-      version: "1.1.0",
-      releaseDate: "2016-02-29",
-      content: `
-      # Recording
-
-      You can now record all of the data sources using the new Record tab. Recorded data can be exported in multiple ways:
-
-      - Save to another app using the Document Picker (iOS 8+ only)
-      - Share using the share extension
-      - Manually copy the file using iTunes File Sharing
-
-      # Bug Fixes and Improvements
-
-      Various bugs have been destroyed and other small improvements have been made, including:
-
-      - The core functionality of Gathered has been rewritten, and is now faster, more stable, and allows each of the data sources to be recorded
-      - Improved the handling of updating the values with the "Update While Scrolling" option enabled - the table should no longer "jump" while scrolling
-      - The "Data Sources" tab has been renamed to "Monitor" to better reflect its use
-      - Added check for whether the device supports Touch ID under the Device data source
-      - Fixed a bug causing adverts to not be displayed in some situations
-      - Fixed a crash that would sometimes occur when using the microphone sensor
-      - Improved reliability of Cell Radio data source when switching carriers
-      - Improved reliability and freshness of data of the GPS data source
-      - Improved accessibility of the Monitor tab
-      - Various other little bug fixes
-      - Adverts are now shown on all tabs
-      - The Update Frequency input will now update the "Second(s)" suffix as you type
+  - Fixed bug in iOS 10 preventing exporting recording to CSV
+  - Updated Microphone data source to not pause audio from other apps
+  - Added link in Settings to the permission settings for Gathered, allowing for easy toggling of permissions
+  - Updated versions of frameworks
+  - Fully removed iAds, relying on only AdMob for adverts
+  - Replaced Device’s "Headphones Attached" with "Audio Output Method"
       `,
     },
     {
       version: "1.1.1",
       releaseDate: "2016-05-05",
       content: `
-      # Additions
+  # Additions
 
-      - Recordings can now be exported to JSON and CSV (this includes those recorded on older versions of Gathered)
-      - Added analytics to help aid future development
+  - Recordings can now be exported to JSON and CSV (this includes those recorded on older versions of Gathered)
+  - Added analytics to help aid future development
 
-      # Fixes
 
-      - Fixed "Thank you for your support! ☺️" line in Settings after buying all In App Purchases having a disclosure indicator
-      - Fixed GPS data source crashing on iOS 7
-      - Fixed In App Purchase related rows on the Settings tab not being refreshed after moving away from the Settings screen
+  # Fixes
 
-      # Changes
 
-      - When a recording is exported to a file, the file name has "Gathered-Recording-" prepended to it now
+  - Fixed "Thank you for your support! ☺️" line in Settings after buying all In App Purchases having a disclosure indicator
+  - Fixed GPS data source crashing on iOS 7
+  - Fixed In App Purchase related rows on the Settings tab not being refreshed after moving away from the Settings screen
+
+
+  # Changes
+
+
+  - When a recording is exported to a file, the file name has "Gathered-Recording-" prepended to it now
       `,
     },
     {
-      version: "1.2.0",
-      releaseDate: "2016-10-18",
+      version: "1.1.0",
+      releaseDate: "2016-02-29",
       content: `
-      - Fixed bug in iOS 10 preventing exporting recording to CSV
-      - Updated Microphone data source to not pause audio from other apps
-      - Added link in Settings to the permission settings for Gathered, allowing for easy toggling of permissions
-      - Updated versions of frameworks
-      - Fully removed iAds, relying on only AdMob for adverts
-      - Replaced Device’s "Headphones Attached" with "Audio Output Method"
+  # Recording
+
+  You can now record all of the data sources using the new Record tab. Recorded data can be exported in multiple ways:
+
+  - Save to another app using the Document Picker (iOS 8+ only)
+  - Share using the share extension
+  - Manually copy the file using iTunes File Sharing
+
+
+  # Bug Fixes and Improvements
+
+
+  Various bugs have been destroyed and other small improvements have been made, including:
+
+  - The core functionality of Gathered has been rewritten, and is now faster, more stable, and allows each of the data sources to be recorded
+  - Improved the handling of updating the values with the "Update While Scrolling" option enabled - the table should no longer "jump" while scrolling
+  - The "Data Sources" tab has been renamed to "Monitor" to better reflect its use
+  - Added check for whether the device supports Touch ID under the Device data source
+  - Fixed a bug causing adverts to not be displayed in some situations
+  - Fixed a crash that would sometimes occur when using the microphone sensor
+  - Improved reliability of Cell Radio data source when switching carriers
+  - Improved reliability and freshness of data of the GPS data source
+  - Improved accessibility of the Monitor tab
+  - Various other little bug fixes
+  - Adverts are now shown on all tabs
+  - The Update Frequency input will now update the "Second(s)" suffix as you type
+      `,
+    },
+    {
+      version: "1.0.1",
+      releaseDate: "2015-11-12",
+      content: `
+  - Removed the Location and Device tabs
+  - Moved the Settings screen to its own tab
+  - Removed the "Disable Device Sleep" option (at Apple's request)
+  - Gathered will now remember which tab you had selected and if you were editing, even when you leave the app closed for a long time
+  - Improved reliability of purchasing and restoring in-app purchases
+  - Fixed multiple crashes on the Settings screen on iOS 7, including:
+
+  - Restoring or purchasing in-app purchases
+  - Tapping on Rate Gathered on iTunes, Feedback, or Privacy Policy
+
+  - Improvements and fixes for iOS 9
+  - Reduced app size
+  - Improved layout of alert to choose how to view a Twitter profile from the About screen
+  - Lots of other little behind the scenes fixes to make Gathered faster and more stable
+      `,
+    },
+    {
+      version: "1.0",
+      releaseDate: "2015-01-23",
+      content: `
+  - Initial release
       `,
     },
   ],
---
+++ pages/apps/[...slug].tsx
@@ -0,0 +1,221 @@
+import { NextPage } from "next"
+import Page from "../../layouts/main"
+import appsLoader from "../../data/loaders/AppsLoader"
+import Head from "next/head"
+import App from "../../models/App"
+import ErrorPage from "../_error"
+import Link from "next/link"
+import AppIcon from "../../components/AppIcon"
+import Markdown from "../../components/Markdown"
+import FormattedDate from "../../components/FormattedDate"
+
+export interface Props {
+  app?: App
+  page: "metadata" | "changelog"
+}
+
+const EntriesPage: NextPage<Props> = ({ app, page }) => {
+  if (!app) {
+    return (
+      <ErrorPage title={"App not found"} statusCode={404}>
+        <Link href="/apps/">
+          <a>Go back to the index of apps</a>
+        </Link>
+        .
+      </ErrorPage>
+    )
+  }
+
+  if (page === "metadata") {
+    return (
+      <Page>
+        <Head>
+          <title>{app.name} :: iOS App by Joseph Duffy</title>
+          <meta
+            name="description"
+            content={app.shortDescription}
+          />
+        </Head>
+        <div className="header">
+          <div className="appIcon">
+            <AppIcon iconURL={app.logoURL} appName={app.name} />
+          </div>
+          <div className="header-content">
+            <h1>{app.name}</h1>
+            <a href={app.url}>
+              <img
+                className="app-store-badge"
+                src="/images/app-store-download-badge.svg"
+                alt={`Download ${app.name} on the App Store`}
+              />
+            </a>
+            <div className="meta-links">
+              <Link href="/apps/[...slug]" as={`/apps/${app.slug}/changelog`}>
+                <a>
+                  Changelog
+                </a>
+              </Link>
+              <span className="divider">•</span>
+              <Link href="/tags/[slug]" as={`/tags/${app.slug}`}>
+                <a>
+                  Related Entries
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <Markdown source={app.fullDescription} escapeHtml={false} />
+        <style jsx>{`
+          .header {
+            display: flex;
+            margin-bottom: 16px;
+          }
+
+          .header-content {
+            display: flex;
+            flex-direction: column;
+            flex: 1;
+          }
+
+          .appIcon {
+            float: left;
+            margin-right: 8px;
+            width: 128px;
+            height: 128px;
+          }
+
+          @media (max-width: 320px) {
+            .appIcon {
+              width: 96px;
+              height: 96px;
+            }
+          }
+
+          .meta-links {
+            padding-top: 8px;
+            font-size: 0.8em;
+            margin-top: auto;
+            padding-bottom: 8px;
+          }
+
+          .meta-links .divider {
+            padding: 0 4px;
+          }
+        `}</style>
+      </Page>
+    )
+  } else {
+    return (
+      <Page>
+        <Head>
+          <title>{app.name} Changelog :: iOS App by Joseph Duffy</title>
+          <meta
+            name="description"
+            content={`Full changelog for ${app.name} iOS App`}
+          />
+        </Head>
+        <h1>{app.name} Changelog</h1>
+        <span>
+        <Link href="/apps/[...slug]" as={`/apps/${app.slug}`}>
+          <a>
+            Back to information about {app.name}
+          </a>
+        </Link>
+        </span>
+        {
+          app.changelogs.map(changelog => {
+            return (
+              <div key={changelog.version}>
+                <h2>{changelog.version}</h2>
+                <FormattedDate date={changelog.releaseDate} />
+                <Markdown source={changelog.content} />
+              </div>
+            )
+          })
+        }
+      </Page>
+    )
+  }
+}
+
+export interface StaticProps {
+  props: Props
+}
+
+interface StaticParams {
+  params: {
+    slug: string[]
+  }
+}
+
+export async function getStaticProps({
+  params,
+}: StaticParams): Promise<StaticProps> {
+  const { slug } = params
+  const appSlug = slug[0]
+  const apps = appsLoader.getApps()
+  const app = apps.find(app => app.slug === appSlug)
+
+  if (!app) {
+    console.warn("Failed to find app with slug", appSlug)
+    return {
+      props: {
+        app: undefined,
+        page: "metadata",
+      },
+    }
+  } else if (slug.length > 1) {
+    if (slug.length !== 2) {
+      // Too many slugs
+      console.warn("Received too many slugs", slug)
+      return {
+        props: {
+          app: undefined,
+          page: "metadata",
+        },
+      }
+    } else {
+      const pageSlug = slug[1]
+
+      if (pageSlug === "changelog") {
+        return {
+          props: {
+            app,
+            page: "changelog",
+          },
+        }
+      } else {
+        console.warn("Unknown slug", slug)
+        return {
+          props: {
+            app: undefined,
+            page: "metadata",
+          },
+        }
+      }
+    }
+  } else {
+    return {
+      props: {
+        app,
+        page: "metadata",
+      },
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const apps = appsLoader.getApps()
+
+  return {
+    fallback: false,
+    paths: apps.flatMap(app => {
+      return [
+        `/apps/${app.slug}`,
+        `/apps/${app.slug}/changelog`,
+      ]
+    }),
+  }
+}
+
+export default EntriesPage
