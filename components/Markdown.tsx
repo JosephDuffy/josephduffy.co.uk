f9d94c538d57489e912e5a1d98249e509b6f0a26
Joseph Duffy <commits@josephduffy.co.uk>
2021-04-22 20:44:34 +0100

    Upgrade to react-markdown 6.0.0

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from "react"
 import ReactMarkdown from "react-markdown"
 import CodeBlock from "./CodeBlock"
 import React from "react"
+import rehypeRaw from "rehype-raw"
 
 interface Props {
   source: string
@@ -11,10 +12,29 @@ interface Props {
 const Markdown: FunctionComponent<Props> = ({ source, escapeHtml }: Props) => {
   return (
     <ReactMarkdown
-      source={source}
-      escapeHtml={escapeHtml}
-      renderers={{ code: CodeBlock }}
-    />
+      rehypePlugins={escapeHtml ? undefined : [rehypeRaw]}
+      components={{
+        // eslint-disable-next-line react/prop-types
+        code({ inline, children, className, ...props }) {
+          const match = /language-(\w+)/.exec(
+            (className as string | undefined) || "",
+          )
+          return !inline && match ? (
+            <CodeBlock
+              language={match[1]}
+              value={String(children).replace(/\n$/, "")}
+              {...props}
+            />
+          ) : (
+            <code className={className as string | undefined} {...props}>
+              {children}
+            </code>
+          )
+        },
+      }}
+    >
+      {source}
+    </ReactMarkdown>
   )
 }
 
