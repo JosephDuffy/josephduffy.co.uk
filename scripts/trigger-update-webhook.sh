3701cc7904a4f1925110f3747cc482573cc80535
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-11 16:34:31 +0100

    Fix prefix being added to openssl output

--- scripts/trigger-update-webhook.sh
+++ scripts/trigger-update-webhook.sh
@@ -9,9 +9,5 @@ PACKAGE="$1"
 SECRET="$2"
 URL="$3"
 DATA="{\"package\":{\"name\":\"$PACKAGE\"}}"
-DIGEST=$(echo -n "$DATA" | openssl dgst -sha256 -hmac "$SECRET")
-echo "DATA: $DATA"
-echo "DIGEST: $DIGEST"
-echo "PACKAGE: $PACKAGE"
-echo "SECRET: $SECRET"
+DIGEST=$(echo -n "$DATA" | openssl dgst -sha256 -hmac "$SECRET" -binary | xxd -p -c 256)
 curl -X POST -H "Content-Type: application/json" -H "X-Hub-Signature-256: sha256=$DIGEST" --data "$DATA" "$URL"
