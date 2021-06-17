cc69e1fa8b24e56598ae0be9313e900924264387
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-17 22:57:59 +0100

    Use blog post dates for sitemap, otherwise omit

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -1,30 +1,29 @@
-import { writeFileSync } from "fs"
-import { glob } from "glob"
+import { promises } from "fs"
+import glob from "glob-promise"
 import { resolve } from "path"
 import { exit } from "process"
-
-if (process.env["WEBSITE_URL"] === undefined) {
-  console.error(
-    "WEBSITE_URL environment variable must be set to generate sitemap",
-  )
-  exit(1)
-}
-
-const websiteURL = process.env["WEBSITE_URL"]
-const now = new Date()
-const date = now.toISOString().substring(0, 10)
-
-const root = resolve(__dirname + "/../.next/server/pages/") + "/"
-const fileExtension = ".html"
-glob(`${root}**/*${fileExtension}`, (err, res) => {
-  if (err) {
-    console.error(err)
+import loader from "../loaders/PostsLoader"
+;(async () => {
+  if (process.env["WEBSITE_URL"] === undefined) {
+    console.error(
+      "WEBSITE_URL environment variable must be set to generate sitemap",
+    )
     exit(1)
   }
 
-  console.log(res)
+  const websiteURL = process.env["WEBSITE_URL"]
 
-  const filteredFiles = res
+  interface URL {
+    readonly uri: string
+    readonly date: Date | null
+  }
+
+  const root = resolve(__dirname + "/../.next/server/pages/") + "/"
+  const fileExtension = ".html"
+  const pages = await glob(`${root}**/*${fileExtension}`)
+  const posts = await loader.getPosts()
+
+  const filteredFiles: URL[] = pages
     .map((file) => file.slice(root.length))
     .filter((file) => {
       return !file.endsWith("404.html")
@@ -34,21 +33,34 @@ glob(`${root}**/*${fileExtension}`, (err, res) => {
     })
     .map((file) => file.replace(/index.html$/, ""))
     .map((file) => file.slice(0, -fileExtension.length))
+    .map((file) => {
+      const postIndex = posts.findIndex((post) => post.url === file)
+      if (postIndex !== -1) {
+        return { uri: file, date: new Date(posts[postIndex].date) }
+      } else {
+        return { uri: file, date: null }
+      }
+    })
 
   let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
 
-  filteredFiles.forEach((file) => {
+  filteredFiles.forEach(({ uri, date }) => {
     sitemap += `
   <url>
-    <loc>${websiteURL}${file}</loc>
-    <lastmod>${date}</lastmod>
+    <loc>${websiteURL}${uri}</loc>`
+
+    if (date !== null) {
+      sitemap += `
+    <lastmod>${date.toISOString().substring(0, 10)}</lastmod>`
+    }
+
+    sitemap += `
   </url>`
   })
 
   sitemap += `
 </urlset>
 `
-
-  writeFileSync(__dirname + "/../public/sitemap.xml", sitemap)
-})
+  await promises.writeFile(__dirname + "/../public/sitemap.xml", sitemap)
+})()
