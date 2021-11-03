efe7e303a1703d3836070765f78f42c4649851d5
Joseph Duffy <commits@josephduffy.co.uk>
2021-11-03 20:46:52 +0000

    Use CSS modules for AppIcon and Card components

--- components/AppIcon.tsx
+++ components/AppIcon.tsx
@@ -1,3 +1,4 @@
+import styles from "./AppIcon.module.css"
 import { Fragment, FunctionComponent } from "react"
 
 interface Props {
@@ -11,22 +12,13 @@ const AppIcon: FunctionComponent<Props> = ({
 }: Props): JSX.Element => {
   return (
     <Fragment>
-      <img src={iconURL} alt={`${appName} Icon`} width="128" height="128" />
-      <style jsx>{`
-        img {
-          filter: drop-shadow(0 0 1px rgba(0, 0, 0, 0.75));
-          border-radius: 22%;
-          width: 96px;
-          height: 96px;
-        }
-
-        @media (min-width: 320px) {
-          img {
-            width: 128px;
-            height: 128px;
-          }
-        }
-      `}</style>
+      <img
+        src={iconURL}
+        alt={`${appName} Icon`}
+        width="128"
+        height="128"
+        className={styles.iconImage}
+      />
     </Fragment>
   )
 }
