3ab971aa030dff1547e137d02c5a26a136d887de
Joseph Duffy <commits@josephduffy.co.uk>
2022-01-23 23:55:47 -0800

    Add Acknowledgements page

---
+++ pages/acknowledgements.tsx
@@ -0,0 +1,47 @@
+import { NextPage } from "next"
+import Page from "../layouts/main"
+import Head from "next/head"
+
+const AcknowledgementsPage: NextPage = () => {
+  const featherLicence = `
+The MIT License (MIT)
+
+Copyright (c) 2013-2017 Cole Bemis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`
+  return (
+    <Page>
+      <Head>
+        <title>Acknowledgements</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <h1>
+        <title>Acknowledgements</title>
+      </h1>
+      <h2>Feather Icons</h2>
+      <p>
+        Download and Compass icons provided by{" "}
+        <a href="https://github.com/feathericons/feather">Feather</a>.
+      </p>
+      <pre dangerouslySetInnerHTML={{ __html: featherLicence }}></pre>
+    </Page>
+  )
+}
+
+export default AcknowledgementsPage
