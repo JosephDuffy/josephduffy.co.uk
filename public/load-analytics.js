a20b003d69b29240cfe3afba4c1f45096d78d3fe
Joseph Duffy <commits@josephduffy.co.uk>
2022-01-23 16:13:32 -0800

    Remove invalid top-level return

--- public/load-analytics.js
+++ public/load-analytics.js
@@ -1,17 +1,15 @@
-if (document.location.hostname !== "josephduffy.co.uk") {
-  // Don't load analytics for noanalytics.josephduffy.co.uk or onion service
-  return;
+// Don't load analytics for noanalytics.josephduffy.co.uk, onion service, development, etc.
+if (document.location.hostname === "josephduffy.co.uk") {
+  var _paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["disableCookies"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://analytics.josephduffy.co.uk/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
 }
-
-var _paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["disableCookies"]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u="https://analytics.josephduffy.co.uk/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '1']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-})();
