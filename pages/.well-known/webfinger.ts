cf3d88808588603c1ab164b8a9e9385a5f2d6fb4
Joseph Duffy <commits@josephduffy.co.uk>
2023-07-12 21:33:16 +0100

    Add basic response for WebFinger requests

---
+++ pages/.well-known/webfinger.ts
@@ -0,0 +1,38 @@
+import { GetServerSideProps } from "next"
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const resource = {
+    subject: "acct:josephduffy@mastodon.social",
+    aliases: [
+      "https://mastodon.social/@josephduffy",
+      "https://mastodon.social/users/josephduffy",
+    ],
+    links: [
+      {
+        rel: "http://webfinger.net/rel/profile-page",
+        type: "text/html",
+        href: "https://mastodon.social/@josephduffy",
+      },
+      {
+        rel: "self",
+        type: "application/activity+json",
+        href: "https://mastodon.social/users/josephduffy",
+      },
+      {
+        rel: "http://ostatus.org/schema/1.0/subscribe",
+        template: "https://mastodon.social/authorize_interaction?uri={uri}",
+      },
+    ],
+  }
+
+  const res = context.res
+  res.setHeader("Content-Type", "application/jrd+json")
+  res.write(JSON.stringify(resource))
+  res.end()
+
+  return { props: {} }
+}
+
+// A page is required to exported to make Next.js happy
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default function WebFinger(): void {}
